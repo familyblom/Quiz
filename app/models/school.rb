5d20fca4446dee10ebd0f7c670963f1143ca8073
Mike Blom <blom@usa.com>
2014-02-05 12:35:17 -0700

    Adds validation for integer fields

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,7 @@
 class School < ActiveRecord::Base
   attr_accessible :left_operand, :operator, :right_operand, :title
-  
+
+  validates :left_operand, :right_operand, presence: {message: "you forgot yo stuff bro"}, 
+  inclusion: { in: 1..9999999999999999999, :message => "requires an integer"}
+
 end
